d32eee2cf8138f11d34058d49c0b13a01ae6f24e
amit <amit.prajapati16007@gmail.com>
2020-02-22 16:15:55 +0530

    First commite
    First commite

---
+++ AspCoreBl/PaymentDetailContext/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using AspCoreBl.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspCoreBl
+{
+    public class PaymentDetailContext : IdentityDbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetail { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}

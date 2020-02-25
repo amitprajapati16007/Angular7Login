74effe7cd2548dc5a081a500a5b5549a332ef7ec
amit <amit.prajapati16007@gmail.com>
2020-02-25 23:34:22 +0530

    IGenericRepository

---
+++ AspCoreBl/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCoreBl.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
---
+++ AspCoreBl/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using AspCoreBl.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspCoreBl.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public PaymentDetailContext _db;
+        public DbSet<T> _dbSet;
+        public GenericRepository(PaymentDetailContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public T GetByID(int id)
+        {
+            return _dbSet
+                  .Find(id);
+        }
+
+        public void Add(T t)
+        {
+            _dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            EntityEntry dbEntityEntry = _dbSet.Attach(t);
+            dbEntityEntry.State = EntityState.Modified;
+        }
+
+        public void Delete(T t)
+        {
+            _dbSet.Remove(t);
+        }
+        public virtual void Save()
+        {
+            _db.SaveChanges();
+        }
+
+
+    }
+}

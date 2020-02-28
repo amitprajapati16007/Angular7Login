4b996799bf8da9a386bd9d120e210b6255bf1e95
amit <amit.prajapati16007@gmail.com>
2020-02-28 18:44:22 +0530

    Pagination learn

--- AspCoreBl/Repositories/PaymentDetailRepository.cs
+++ AspCoreBl/Repositories/PaymentDetailRepository.cs
@@ -5,10 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using AspCoreBl.Utility;
 
 namespace AspCoreBl.Repositories
 {
-    public class PaymentDetailRepository: GenericRepository<PaymentDetail>, IPaymentDetailRepository
+    public class PaymentDetailRepository : GenericRepository<PaymentDetail>, IPaymentDetailRepository
     {
         private readonly PaymentDetailContext _db;
 
@@ -16,14 +17,54 @@ namespace AspCoreBl.Repositories
         {
             _db = db;
         }
-
-        public void Save(PaymentDetailDTO dto) {
+     
+        public List<PaymentDetailDTO> GetList(Pager p)
+        {
+            List<PaymentDetailDTO> list=new List<PaymentDetailDTO>();
+            var objPaymentDetail = (from s in _db.PaymentDetail
+                                    select new PaymentDetailDTO()
+                                    {
+                                        PMID = s.PMID,
+                                        CardOwnerName = s.CardOwnerName,
+                                        CardNumber = s.CardNumber
+                                    }
+                            ).AsQueryable();
+            switch (p.sortOrder)
+            {
+                case "asc":                    
+                    switch (p.sortColumnName)
+                    {
+                        case "CardOwnerName":
+                            objPaymentDetail = objPaymentDetail.OrderBy(s => s.CardOwnerName);
+                            break;
+                        case "CardNumber":
+                            objPaymentDetail = objPaymentDetail.OrderBy(s => s.CardNumber);
+                            break;
+                    }
+                    break;
+                case "desc":
+                    switch (p.sortColumnName)
+                    {
+                        case "CardOwnerName":
+                            objPaymentDetail = objPaymentDetail.OrderByDescending(s => s.CardOwnerName);
+                            break;
+                        case "CardNumber":
+                            objPaymentDetail = objPaymentDetail.OrderByDescending(s => s.CardNumber);
+                            break;
+                    }
+                    break;
+            }
+           
+            return list;
+        }
+        public void Save(PaymentDetailDTO dto)
+        {
             var obj = _db.PaymentDetail.FirstOrDefault(p => p.PMID == dto.PMID);
-            if (obj==null)
+            if (obj == null)
             {
                 obj = new PaymentDetail
                 {
-                 
+
                 };
                 _db.PaymentDetail.Add(obj);
             }
